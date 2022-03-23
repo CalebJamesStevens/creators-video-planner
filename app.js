732e269ffc36c544812d5e725a8284c551e000cb
Caleb Stevens <calebjamesstevens219@gmail.com>
2022-03-23 16:46:01 -0700

    Fix bugs

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
+        console.warn(path.resolve(__dirname, 'client', 'build', 'index.html'));
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
